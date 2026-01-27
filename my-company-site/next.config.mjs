bdda3b1342a9eb93f0b52dc224c2c535ed4925ad
creodedulab <apdlgy123@gmail.com>
2026-01-27 09:53:24 +0900

    강사16추가

--- my-company-site/next.config.mjs
+++ my-company-site/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  
+  // ▼▼▼ 여기가 핵심입니다! ▼▼▼
+  // 만약 basePath: '/company-intro' <-- 이런 줄이 있다면 무조건 지우세요!
+  // assetPrefix <-- 이런 것도 있으면 지우세요!
+  
   images: {
     unoptimized: true,
   },
